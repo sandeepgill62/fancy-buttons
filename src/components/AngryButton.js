f84d31784f166cb8c77a57d3da7d93ad7269f87a
Sandeep Gill <sandeepgill62@gmail.com>
2023-08-08 05:09:37 +0000

    Adding State to Our Components

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function AngryButton () {
+
+  const [anger, setAnger] = useState(0);
+
+
   return (
-    <button className="AngryButton">
-      {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-      {/* When the threshold is not reached */}<span>Rawr!</span>
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr!</span>}
     </button>
   );
 }
